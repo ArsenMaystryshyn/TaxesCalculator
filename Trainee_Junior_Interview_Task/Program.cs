b945d6a83996d132bed102de0478d75143848ae8
Arsen Maystryshyn <arsen_maystryshyn@epam.com>
2018-05-17 14:58:03 +0300

    Taxes calculation. Done.

---
+++ Trainee_Junior_Interview_Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Trainee_Junior_Interview_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter Basic Salary");
+                double basicSalary = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter Current Salary");
+                double currentSalary = double.Parse(Console.ReadLine());
+
+                TaxesCalculator taxesCalculator = new TaxesCalculator();
+
+                var taxes = taxesCalculator.Calculate(basicSalary, currentSalary);
+                var currentSalaryNetto = currentSalary - taxes;
+                Console.WriteLine($"Basic Salary: {basicSalary}");
+                Console.WriteLine($"Current Salary (Bruto): {currentSalary}");
+                Console.WriteLine($"Taxes to pay: {taxes}");
+                Console.WriteLine($"Current Salary (Netto): {currentSalaryNetto}");
+
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ Trainee_Junior_Interview_Task/TaxesCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Trainee_Junior_Interview_Task
+{
+    class TaxesCalculator
+    {
+   
+        public TaxesCalculator()
+        {
+        }
+
+        public double Calculate(double basicSalary,double currentSalary)
+        {
+            if (basicSalary <= 0 || currentSalary <= 0)
+            {
+                throw new Exception("Incorrect data");
+            }
+
+            double taxes = 0;
+            if (currentSalary <= basicSalary)
+            {
+                return taxes;
+            }
+            if (currentSalary > basicSalary && basicSalary <= 5 * basicSalary)
+            {
+                taxes = currentSalary * 15 / 100;
+                return taxes;
+            }
+            if (currentSalary > 5 * basicSalary && 5 * basicSalary <= 10 * basicSalary)
+            {
+                taxes = (currentSalary - 5 * basicSalary) * 17 / 100;
+                return taxes;
+            }
+            if (currentSalary > 10 * basicSalary)
+            {
+                taxes = (currentSalary - 5 * basicSalary) * 20 / 100;
+                return taxes;
+            }
+            return taxes;
+        }
+
+    }
+}
